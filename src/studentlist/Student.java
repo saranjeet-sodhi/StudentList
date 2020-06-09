35ac0b53e5036adbd2d5226ef10edf4b7c16cc79
Saranjeet <Saranjeet@Saranjeet.mshome.net>
2020-06-08 22:01:24 -0400

    Adding Getters and Setters for new String field ID

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -8,9 +8,18 @@ package studentlist;
 public class Student {
     
     private String name;
+    private String id;
 
     public Student(String name) {
         this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
